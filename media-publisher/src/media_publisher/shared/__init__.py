d525e31820c8eee50e0f66f6a55365bd86f45adc
yyy <yyy@no-replay.com>
2026-03-10 21:38:40 +0800

    delete media-publisher, delete creator

--- media-publisher/src/media_publisher/shared/__init__.py
+++
@@ -1,6 +0,0 @@
-"""Shared utility modules."""
-
-from .io import atomic_write_json, atomic_write_text
-from .security import sanitize_identifier
-
-__all__ = ["atomic_write_json", "atomic_write_text", "sanitize_identifier"]
